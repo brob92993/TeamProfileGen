c034234285511e94e90845b9638ee737ea85dbe5
Brian Robinson <brob92993@gmail.com>
2021-07-05 09:30:55 -0400

    added code to the Engineer.test.js

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,20 @@
+const Engineer = require('../lib/Engineer');
+
+
+it("Can set the Github account via the constructor", () => {
+    const testValue = "GithubUser";
+    const e = new Engineer("Brian", 1, "test@test.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
+it("getRole() should return \"Engineer\"", ()=> {
+    const testValue = "Engineer";
+    const e = new Engineer("Brian", 1, "test@test.com", "GithubUser");
+    expect(e.getRole()).toBe(testValue);
+})
+
+it("can get the Github username from getGithub()", () =>{
+    const testValue = "GithubUser";
+    const e = new Engineer ("Brian", 1, "test@test.com", testValue);
+    exptect(e.getGithub()).toBe(testValue);
+})
