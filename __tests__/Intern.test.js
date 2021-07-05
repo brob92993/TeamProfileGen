d9f7cf07527988a5e6b7314adf4f9233fab62a2f
Brian Robinson <brob92993@gmail.com>
2021-07-05 09:59:15 -0400

    updated code for Intern.test.js

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require('../lib/Intern');
+
+it ("can set school via the constructor", () => {
+    const testValue = "UNCC";
+    const e = new Intern("Brian", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+it ("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";a
+    const e = new Intern("Brian", 1, "test@test.com", "UNCC");
+    expect(e.getRole()).toBe(testValue);
+});
+
+it ("gets school from getSchool()", () => {
+    const testValue = "UNCC";
+    const e = new Intern("Brian", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
